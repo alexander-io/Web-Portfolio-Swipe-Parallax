25934466dd3e7e773bd3e065376fffe6d22e88e6
alexander-io <abharris@pugetsound.edu>
2018-09-03 15:32:25 -0700

    update routes, move dependencies  to server side, define event listeners on document load

--- server.js
+++ server.js
@@ -15,7 +15,29 @@ app.get('/out_full.mp4', function(req, res) {
   res.sendFile(__dirname + '/media/output_full.mp4')
 })
 
+app.get('/materialize.js', function(req, res) {
+  res.sendFile(__dirname + '/node_modules/materialize-css/dist/js/materialize.js')
+})
 
+app.get('/materialize.css', function(req, res) {
+  res.sendFile(__dirname + '/node_modules/materialize-css/dist/css/materialize.css')
+})
+
+app.get('/highlight.min.js', function(req, res) {
+  res.sendFile(__dirname + '/client/js/highlight.min.js')
+})
+
+app.get('/swiper.js', function(req, res) {
+  res.sendFile(__dirname + '/client/js/swiper.js')
+})
+
+app.get('/swiper.css', function(req, res) {
+  res.sendFile(__dirname + '/client/css/swiper.css')
+})
+
+app.get('/jquery.js', function(req, res) {
+  res.sendFile(__dirname + '/node_modules/jquery/dist/jquery.js')
+})
 
 app.listen(port, () => {
   console.log('listening on ' + port)
